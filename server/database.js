bde98eaa7cb7a8a82a6750091f60c39067d1aed2
Raihan <raihanregita@gmail.com>
2025-12-05 11:31:04 +0700

    test

--- server/database.js
+++ server/database.js
@@ -1,14 +1,8 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_HOST,
-        dialect: "mysql",
-        logging: false,
-    }
-);
+const db = new Sequelize("DB_NAME", "DB_USER", "DB_PASSWORD", {
+    host: "mysql.hostinger.com",
+    dialect: "mysql",
+});
 
 export default db;

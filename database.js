5c8aaa654d3584da10da74ab8d4131d11ee12a1d
Hyansz <114051543+Hyansz@users.noreply.github.com>
2025-12-05 11:29:40 +0700

    Delete database.js

--- database.js
+++
@@ -1,8 +0,0 @@
-import { Sequelize } from "sequelize";
-
-const db = new Sequelize("DB_NAME", "DB_USER", "DB_PASSWORD", {
-    host: "mysql.hostinger.com",
-    dialect: "mysql",
-});
-
-export default db;

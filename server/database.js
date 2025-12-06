8a1dd72e8b922319100046ffc39b72703c0a924d
Raihan <raihanregita@gmail.com>
2025-12-06 09:15:42 +0700

    test

--- server/database.js
+++
@@ -1,8 +0,0 @@
-import { Sequelize } from "sequelize";
-
-const db = new Sequelize("DB_NAME", "DB_USER", "DB_PASSWORD", {
-    host: "https://invitaitionspi.id/",
-    dialect: "mysql",
-});
-
-export default db;
